e46f81af1256c1cc9339f657015b65680c95cb62
Mariappan05 <rammariappan05@gmail.com>
2024-12-12 12:19:19 +0530

    Add project files.

---
+++ FoodOrderApi/Controllers/FoodItemsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FoodOrderApi.Models;
+
+namespace FoodOrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodItemsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public FoodItemsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/FoodItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FoodItem>>> GetFoodItems()
+        {
+            return await _context.FoodItems.ToListAsync();
+        }
+    }
+}
